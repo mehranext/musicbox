ae31c08b5ec12305715f0d758d55daf81bfb54dd
Mehranxt <132151347+mehranext@users.noreply.github.com>
2023-05-08 13:14:00 +0430

    we create our method for playing songs in conroller and for safe test we use the try catch

--- lib/controller/player_controller.dart
+++ lib/controller/player_controller.dart
@@ -1,15 +1,27 @@
 import 'package:get/get.dart';
+import 'package:just_audio/just_audio.dart';
 import 'package:on_audio_query/on_audio_query.dart';
 import 'package:permission_handler/permission_handler.dart';
 
 class PlayerController extends GetxController {
+//methods
   final audioQuery = OnAudioQuery();
+  final audioPlayer = AudioPlayer();
 
   @override
   void onInit() {
     super.onInit();
 
     checkPermission();
+  }
+
+  playsong(String? uri) {
+    try {
+      audioPlayer.setAudioSource(AudioSource.uri(Uri.parse(uri!)));
+      audioPlayer.play();
+    } on Exception catch (e) {
+      print(e.toString());
+    }
   }
 
   checkPermission() async {
